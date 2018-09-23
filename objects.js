02893ab58d5c0de78ffa4f8a2c52cf2bd9bd767b
phamthaop.9 <phamthaop.9@gmail.com>
2018-09-23 02:53:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { kanye: 'I am a God', jcole: 'Morning', jayz: '99 Problems' } 
 
 function updatePlaylist (playlist,artistName,songTitle) {
-  
+  playlist.artistName = 'songTitle'
+  return playlist
 }
