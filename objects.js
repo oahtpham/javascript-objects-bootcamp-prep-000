7ae55c6d51a0121dd46791458139025d8f408606
phamthaop.9 <phamthaop.9@gmail.com>
2018-09-23 03:02:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = { Slowdive: 'I am a God', My Bloody Valentine: 'Morning' } 
+var playlist = { 
+  Slowdive: 'I am a God', 
+  MyBloodyValentine:'Morning' 
+}
 
 function updatePlaylist (playlist,artistName,songTitle) {
   playlist.artistName = 'Phil Ochs'
